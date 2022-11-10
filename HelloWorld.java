3b5e6b50d41e56de632051fd835f2c589e67b091
EhsunD <99817926+EhsunD@users.noreply.github.com>
2022-11-10 13:56:40 +0330

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld{
   public static void main(string args[]){
     system.out.println("Hello World!");
     //this is comment
-    system.out.println("hiii")
+    system.out.println("hiii");
   }
 }
 
